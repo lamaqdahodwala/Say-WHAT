20650d6df639de31eeff62625a6574b47d478644
Lamaq Dahodwala <74932012+lamaqdahodwala@users.noreply.github.com>
2021-02-26 15:52:06 -0500

    fdsafdsafdsa

--- src/public/script.js
+++ src/public/script.js
@@ -24,6 +24,26 @@ function post(){
     unhide('postscreen')
     hide('thingstodo')
     hide('title')
+    
+}
+function sendtoserver(){
+    const data = {
+        title: String(document.getElementsByTagName('textarea')[0].value),
+        contents: String(document.getElementsByTagName('textarea')[1].value)
+    }
+    const request = new Request('https://saywhatserver.lamaqdahodwala.repl.co/post', {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    }) 
+    fetch(request)
+    .then(res => res.json())
+    .then(res => console.log(res))
+
+    alert(document.getElementsByTagName('textarea')[0].value)    
+    
 }
 
 function hide(element){
@@ -33,3 +53,4 @@ function hide(element){
 function unhide(element){
     document.getElementById(element).hidden = false
 }
+
